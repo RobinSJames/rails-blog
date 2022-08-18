f3620881b86e2c964702bdafc51bb0ff783ece15
RobinSJames <stuartrobinmcculloch@gmail.com>
2022-08-18 18:33:48 +0200

    Added the show action for an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,10 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  # note the singular reference (@article)
+  def show
+    @article = Article.find(params[:id])
+  end
+
 end
